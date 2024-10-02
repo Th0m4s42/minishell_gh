e8aab64692a89b1c6431d6c82041da4ad27322b7
Thomas <thbasse@student.42.fr>
2024-10-02 17:01:14 +0200

    u

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 15:10:13 by thbasse           #+#    #+#             */
-/*   Updated: 2024/10/02 16:24:22 by thbasse          ###   ########.fr       */
+/*   Updated: 2024/10/02 16:47:03 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 int	main(void)
 {
-	char **array_to_parse;
+	char *rl_value;
 	
+	rl_value = NULL;
 	while (1)
 	{
-		array_to_parse = ft_split(readline("minishell:"), ' ');
-		free(array_to_parse);
+		rl_value = readline("minishell:");
+		add_history(rl_value);
+		free(rl_value);
 	}
 }
