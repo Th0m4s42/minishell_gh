5131531fb1bda7f1ae675ca49fb3cee4901fde66
Thomas <thbasse@student.42.fr>
2024-10-02 14:28:46 +0200

    u

---
+++ Makefile
@@ -0,0 +1,46 @@
+NAME := minishell
+CC := cc
+FLAGS := -Wall -Werror -Wextra -g
+
+Black=\033[0;30m
+Red=\033[0;31m
+Green=\033[0;32m
+Yellow=\033[0;33m
+Blue=\033[0;34m
+Purple=\033[0;35m
+Cyan=\033[0;36m
+White=\033[0;37m
+
+SRC_DIR := srcs/
+OBJ_DIR := objs/
+
+SRCS := \
+	$(SRC_DIR)main.c \
+
+INCLUDES := -I includes
+
+OBJS := $(patsubst $(SRC_DIR)%.c,$(OBJ_DIR)%.o,$(SRCS))
+
+$(OBJ_DIR)%.o : $(SRC_DIR)%.c
+	@mkdir -p $(@D)
+	@$(CC) $(FLAGS) $(INCLUDES) -c -o $@ $<
+	@echo "$(Cyan)Compiling $<...$(White)"
+
+$(NAME) : $(OBJS)
+	@$(CC) $(FLAGS) $(INCLUDES) $(OBJS) -o $(NAME)
+	@echo "$(Green)$(NAME) CREATED $(White)"
+
+all : $(NAME)
+
+clean :
+	@echo "$(Red)Cleaning object files...$(White)"
+	@rm -f $(OBJS)
+	@rm -rf $(OBJ_DIR)
+
+fclean : clean
+	@rm -f $(NAME)
+	@echo "$(Red)$(NAME) DELETED $(White)"
+
+re : fclean all
+
+.PHONY: re clean fclean
---
+++ includes/minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 14:13:03 by thbasse           #+#    #+#             */
+/*   Updated: 2024/10/02 14:25:34 by thbasse          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+#endif
