c9ab04813724b12b199c10ca96e0556809a821b5
Thomas <thbasse@student.42.fr>
2024-10-09 13:21:06 +0200

    u

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 14:13:03 by thbasse           #+#    #+#             */
-/*   Updated: 2024/10/02 16:22:01 by thbasse          ###   ########.fr       */
+/*   Updated: 2024/10/09 13:15:05 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,36 @@
 # include <curses.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+
+# define CMD 101
+# define SEP 102
+# define RED 103
+# define PIP 104
+
+////////////////////////////////////////////////////////////////////////////////
+//								STRUCTURES									  //
+////////////////////////////////////////////////////////////////////////////////
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+typedef struct s_line
+{
+	char			**env;
+	char			*line;
+	char			*cmd;
+	char			**args;
+	char			*sep;
+}					t_line;
+
+typedef struct s_token
+{
+	char			*string;
+	int				token;
+}					t_token;
 
 ////////////////////////////////////////////////////////////////////////////////
 //								FONCTIONS									  //
