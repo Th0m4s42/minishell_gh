208857f61c64a88e9edcade4c83662c1b8c95e19
Thomas <thbasse@student.42.fr>
2024-10-11 15:14:17 +0200

    u

--- srcs/env/env.c
+++ srcs/env/env.c
@@ -6,33 +6,56 @@
 /*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 13:25:49 by thbasse           #+#    #+#             */
-/*   Updated: 2024/10/10 15:58:55 by thbasse          ###   ########.fr       */
+/*   Updated: 2024/10/11 15:13:08 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-int	get_env(t_env *save, char **envp)
+char	*get_env_variable_name(char *envp, char *name)
 {
 	int	i;
-	int	j;
-	int	k;
 
 	i = 0;
-	while (envp[i])
-	{
-		j = 0;
-		while (envp[i][j] != '=')
-			j++;
-		save->name = malloc(sizeof(char) * (j + 1));
-		ft_strlcpy(save->name, envp[i], j);
-		k = j;
-		while (envp[i][k] != '\0')
-			k++;
-		save->value = malloc(sizeof(char) * (k - j + 1));
-		ft_strlcpy(save->value, *(envp + j), k - j);
-		new_env_node(save->name, save->value);
+	while (envp[i] && envp[i] != '=')
 		i++;
+	ft_strlcpy(name, envp, i);
+	return (name);
+}
+
+char	*get_env_variable_value(char *envp, char *value)
+{
+	int	i;
+	
+	i = 0;
+	while (envp[i] && envp[i] != '=')
+		i++;
+	if (envp[i] == '=')
+		i++;
+	ft_strlcpy(value, &envp[i], ft_strlen(&envp[i]) + 1);
+	return (value);
+}
+
+t_env	*get_env(char **envp)
+{
+	t_env	*env;
+	char	name[256];
+	char	value[256];
+
+	env = NULL;
+	while(envp)
+	{
+		get_env_variable_name(*envp, name);
+		get_env_variable_value(*envp, value);
+		if (env == NULL)
+			env = new_env_node(name, value);
+		else
+		{
+			while(env)
+				env = env->next;
+			env->next = new_env_node(name, value);
+		}
+		envp++;
 	}
-	return (1);
+	return (env);
 }
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 15:10:13 by thbasse           #+#    #+#             */
-/*   Updated: 2024/10/10 15:50:40 by thbasse          ###   ########.fr       */
+/*   Updated: 2024/10/11 15:07:41 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 int	main(int argc, char **argv, char **envp)
 {
 	char *rl_value;
-	t_env *env;
+	t_env *env = NULL;
 
 	(void)argv;
-	env = malloc(sizeof(t_env));
 	rl_value = NULL;
 	if (argc != 1)
 		return (EXIT_FAILURE);
-	get_env(env, envp);
-	while(env)
+	get_env(envp);
+	while (envp)
 	{
-		printf("%s = %s\n ", env->name, env->value);
-		env = env->next;
+		printf("%s=%s", env->name, env->value);
+		envp++;
 	}
+	free_env_list(&env);
 	// while (1)
 	// {
 	// 	rl_value = readline("minishell:");
