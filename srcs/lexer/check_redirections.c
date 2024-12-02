7fd1751e48921f4e18f62ed26ab7c65a11787bdf
thbasse <thbasse@42.student.fr>
2024-12-02 15:17:35 +0100

    u

--- srcs/lexer/check_redirections.c
+++ srcs/lexer/check_redirections.c
@@ -6,7 +6,7 @@
 /*   By: thbasse <thbasse@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 14:21:00 by thbasse           #+#    #+#             */
-/*   Updated: 2024/12/02 15:05:32 by thbasse          ###   ########.fr       */
+/*   Updated: 2024/12/02 15:17:07 by thbasse          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ bool	check_redirection(char *token, t_token *tok)
 
 bool	check_infile(char *token, t_token *tok)
 {
-	if (tok != NULL && token[0] == '<' && token[1] == '\0' &&
+	(void)token;
+	if (tok != NULL && tok->value[0] == '<' && tok->value[1] == '\0' &&
 		tok->type == REDIRECTION)
 		return (true);
 	return (false);
@@ -31,15 +32,17 @@ bool	check_infile(char *token, t_token *tok)
 
 bool	check_heredoc(char *token, t_token *tok)
 {
-	if (tok != NULL && token[0] == '<' && token[1] == '<' &&
-		token[2] == '\0' && tok->type == REDIRECTION)
+	(void)token;
+	if (tok != NULL && tok->value[0] == '<' && tok->value[1] == '<' &&
+		tok->value[2] == '\0' && tok->type == REDIRECTION)
 		return(true);
 	return (false);
 }
 
 bool	check_outfile(char *token, t_token *tok)
 {
-	if (tok != NULL && token[0] == '>' && token[1] == '\0' &&
+	(void)token;
+	if (tok != NULL && tok->value[0] == '>' && tok->value[1] == '\0' &&
 		tok->type == REDIRECTION)
 		return(true);
 	return (false);
@@ -47,8 +50,9 @@ bool	check_outfile(char *token, t_token *tok)
 
 bool	check_append(char *token, t_token *tok)
 {
-	if (tok != NULL && token[0] == '>' && token[1] == '>' &&
-		token[2] == '\0' && tok->type == REDIRECTION)
+	(void)token;
+	if (tok != NULL && tok->value[0] == '>' && tok->value[1] == '>' &&
+		tok->value[2] == '\0' && tok->type == REDIRECTION)
 		return(true);
 	return (false);
 }
